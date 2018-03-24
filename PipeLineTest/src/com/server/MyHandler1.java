651ea16cecf66fa745d8a7ae3bee4e75fc2cabe6
yefeixue <djl150625@163.com>
2018-03-24 21:36:58 +0800

    消息传递： ctx.sendUpstream(new UpstreamMessageEvent(ctx.getChannel(), "abc", e.getRemoteAddress()));

--- PipeLineTest/src/com/server/MyHandler1.java
+++ PipeLineTest/src/com/server/MyHandler1.java
@@ -16,7 +16,7 @@ public class MyHandler1 extends SimpleChannelHandler {
 		byte[] array = buffer.array();
 		String message = new String(array);
 		System.out.println("handler1:" + message);
-		
+	
 		//传递
 		ctx.sendUpstream(new UpstreamMessageEvent(ctx.getChannel(), "abc", e.getRemoteAddress()));
 		ctx.sendUpstream(new UpstreamMessageEvent(ctx.getChannel(), "efg", e.getRemoteAddress()));
