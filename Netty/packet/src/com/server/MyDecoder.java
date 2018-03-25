4bffe103d77bbd7c23554fe00bcc5a974db109b0
yefeixue <djl150625@163.com>
2018-03-25 17:03:18 +0800

    整理

---
+++ Netty/packet/src/com/server/MyDecoder.java
@@ -0,0 +1,47 @@
+package com.server;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.frame.FrameDecoder;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyDecoder extends FrameDecoder {
+
+
+	AtomicInteger atomicInteger = new AtomicInteger();
+
+	@Override
+	protected Object decode(ChannelHandlerContext ctx, Channel channel, ChannelBuffer buffer) throws Exception {
+		System.out.println(buffer.capacity()+"    "+atomicInteger.incrementAndGet());
+
+		if(buffer.readableBytes() > 4){
+
+			if(buffer.readableBytes() > 2048){
+				buffer.skipBytes(buffer.readableBytes());
+			}
+
+
+			//标记
+			buffer.markReaderIndex();
+			//长度
+			int length = buffer.readInt();
+
+			if(buffer.readableBytes() < length){
+				buffer.resetReaderIndex();
+				//缓存当前剩余的buffer数据，等待剩下数据包到来
+				return null;
+			}
+
+			//读数据
+			byte[] bytes = new byte[length];
+			buffer.readBytes(bytes);
+			//往下传递对象
+			return new String(bytes);
+		}
+		//缓存当前剩余的buffer数据，等待剩下数据包到来
+		return null;
+	}
+
+}
